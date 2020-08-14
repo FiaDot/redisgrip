72181def0ce7f52a7dfbe9baaa6c5376e829474c
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2020-08-14 15:13:25 +0900

    일단 리다이렉트로 해결

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom/esm/react-router-dom';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}> to Counter</Link>
-      <Link to={routes.ADDSERVER}> to Add Servers</Link>
-      <Link to={routes.REDISGRIP}> to Servers</Link>
-    </div>
+    <Redirect push to="/redisgrip" />
   );
 }
