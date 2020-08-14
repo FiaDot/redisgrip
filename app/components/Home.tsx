ce3c5030fc2095535ebc05ed5d2343c2c617b138
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2020-08-14 15:18:19 +0900

    현재 상태 정리 주석 추가

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { Redirect } from 'react-router-dom/esm/react-router-dom';
 
 export default function Home(): JSX.Element {
+
+  /*
+   TODO : react-router-dom 에 대한 이해도를 높일 때 까지 HomePage에서 Home을 부르고
+      Redirect로 Servers페이지를 부르는 형태를 유지 한다.
+      변경시 packaging 성공 후 실행시 제대로 동작 하지 않는 부분이 있다.
+   */
+
   return (
     <Redirect push to="/redisgrip" />
   );
