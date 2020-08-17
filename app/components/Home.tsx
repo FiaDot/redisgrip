a99bc38937a3d9e1970302ef32d57f916812ff54
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2020-08-17 11:20:11 +0900

    프로젝트 정보 수정

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect } from 'react-router-dom/esm/react-router-dom';
 
 export default function Home(): JSX.Element {
-
   /*
    TODO : react-router-dom 에 대한 이해도를 높일 때 까지 HomePage에서 Home을 부르고
       Redirect로 Servers페이지를 부르는 형태를 유지 한다.
--- app/containers/RedisGripPage.tsx
+++ app/containers/RedisGripPage.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Servers from '../features/servers/Servers';
 import ServerSheet from '../features/servers/ServerSheet';
 
 export default function RedisGripPage() {
-  //return <Servers />;
   return <ServerSheet />
 }
