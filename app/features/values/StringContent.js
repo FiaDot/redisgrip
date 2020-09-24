097dfa80f8e2ef434c83b4eb328083e9ba067472
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2020-09-24 20:54:31 +0900

    string에서 현재 value 는 codemirror 사용. 이전 value는 단순 출력

--- app/features/values/StringContent.js
+++ app/features/values/StringContent.js
@@ -14,6 +14,7 @@ import TimeNoComponent from './TimeNoComponent';
 import ValueDialog from './ValueDialog';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
 
+
 export default function StringContent() {
   const classes = useValueStyles();
 
@@ -26,27 +27,29 @@ export default function StringContent() {
     dispatch(addString({ key, value }));
   };
 
-  const showValues = (key, value) => (
-    // TODO : key가 들어가야 에러가 안난다... 넣었을때 고유번호 같은걸 하나 넣어야 할듯!!
-    // <Typography
-    //   className={classes.title}
-    //   color="textSecondary"
-    //   align="center"
-    //   key={`${key}_${history.no}`}
-    // >
-    //   {history.no} {history.value} {history.time}
-    // </Typography>
+
+  const showHistoryValues = (key, value) => (
     <div className={classes.paper} key={`${key}_${value.no}`}>
       <TimeNoComponent time={value.time} no={value.no} />
 
       <Paper elevation={3}>
-        {/*<Typography>{value.value}</Typography>*/}
+        <Typography>{value.value}</Typography>
+      </Paper>
+    </div>
+  );
 
+  const showValues = (key, value) => (
+    <div className={classes.paper} key={`${key}_${value.no}`}>
+      <TimeNoComponent time={value.time} no={value.no} />
+
+      <Paper elevation={3}>
         <CodeMirror
           value={value.value}
           options={{
-            mode: 'xml',
-            theme: 'material',
+            mode: 'jsx',
+            theme: 'darcula',
+            lint: true,
+            gutters: ['CodeMirror-lint-json'],
             lineNumbers: true,
             readOnly: true,
             showCursorWhenSelecting: false,
@@ -63,8 +66,10 @@ export default function StringContent() {
     <div>
       {stringRecords.map((record) =>
         record.key === selectKey
-          ? record.values.map((valueRecord) =>
-              showValues(record.key, valueRecord)
+          ? record.values.map((valueRecord, index) =>
+            index === 0
+              ? showValues(record.key, valueRecord)
+              : showHistoryValues(record.key, valueRecord)
             )
           : ''
       )}
