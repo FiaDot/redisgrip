a0807f205e3a74033ad8311a8c3883755de0552a
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2020-09-25 16:22:33 +0900

    파일 저장 다이얼로그

--- app/features/keys/MigrationToolbar.js
+++ app/features/keys/MigrationToolbar.js
@@ -7,6 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import SystemUpdateAltOutlinedIcon from '@material-ui/icons/SystemUpdateAltOutlined';
 import BackupOutlinedIcon from '@material-ui/icons/BackupOutlined';
 import fs from 'fs';
+const {dialog} = require('electron').remote;
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -23,13 +24,31 @@ export default function MigrationToolbar() {
   const onExport = () => {
     console.log('onExport');
 
-    try {
-      fs.writeFileSync('myfile.txt', 'the text to write in the file', 'utf-8');
-    }
-    catch(e) {
-      console.log(`write file error=${e}`);
-      //alert('Failed to save the file !');
-    }
+    // try {
+    //   fs.writeFileSync('myfile.txt', 'the text to write in the file', 'utf-8');
+    // }
+    // catch(e) {
+    //   console.log(`write file error=${e}`);
+    //   //alert('Failed to save the file !');
+    // }
+    //
+
+    // TODO : file name generator
+    var options = {
+      title: 'Save Dump File',
+      defaultPath: 'my_filename',
+      buttonLabel: 'Save',
+      filters: [
+        { 'name': 'RedisGrip Dump', extensions: ['rgd',]},
+      ],
+    };
+
+    dialog.showSaveDialog(options).then(file => {
+      if ( !file.canceled ) {
+        console.log(file.filePath.toString());
+        fs.writeFileSync(file.filePath.toString(), 'hello world', 'utf-8');
+      }
+    });
   };
 
   const onImport = () => {
